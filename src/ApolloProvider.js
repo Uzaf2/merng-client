fc58303fd8fe1a8d49bfb89d1a09884a627c0db9
Usman Zafar <Uzman.Zafar1@gmail.com>
2020-12-22 13:38:35 +1100

    First commit

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://protected-brook-11387.herokuapp.com/'
 })
 
 const authLink = setContext(()=>{
